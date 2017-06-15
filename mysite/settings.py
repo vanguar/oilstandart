36e2c17437cd054cc1e92813501ba60cb93f9be1
tzvanguardia@gmail.com <tzvanguardia@gmail.com>
2017-06-15 11:24:15 +0300

    Changed the HTML

--- mysite/settings.py
+++ mysite/settings.py
@@ -110,21 +110,3 @@ EMAIL_HOST_PASSWORD = 'vovusiklenusik'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = 'tzvanguardia@gmail.com'
-
-FILE_CHARSET='utf-8'
-
-DEFAULT_CHARSET='utf-8'
-
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
-
-DEBUG = False
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
